d09e9bf8b11a24c56ab80d2f05d48eae1f7aba92
bitwiseberto <rpn241@nyu.edu>
2024-03-22 16:38:34 -0400

    fcms mini game

--- src/stores.js
+++ src/stores.js
@@ -3,7 +3,7 @@ import { readable } from 'svelte/store';
 import { csv } from 'd3-fetch';
 
 export const statements = readable([], set => {
-    csv('/surprising_statistics_statements_and_answers.csv').then(data => {
+    csv('/fcms_stats.csv').then(data => {
         // Assuming the CSV has columns named 'statement', 'answer', and 'source'
         const formattedData = data.map(d => ({
             statement: d.statement,
